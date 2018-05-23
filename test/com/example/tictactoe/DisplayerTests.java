d2e954ff980c64ca35341c6e1b5e1647d4fa18a0
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-05-23 17:07:26 +0100

    Refactor: dried tests in Displayer.

--- test/com/example/tictactoe/DisplayerTests.java
+++ test/com/example/tictactoe/DisplayerTests.java
@@ -4,18 +4,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DisplayerTests {
 
     public Displayer displayer;
+    public Board board;
+    public Validator validator;
 
     @BeforeEach
-    public void instantiateBoard() { displayer = new Displayer(); }
+    public void instantiate() {
+        displayer = new Displayer();
+        board = new Board();
+        validator = new Validator();
+        Collections.addAll(board.places, 0, 1, 2, 3, 4, 5, 6, 7, 8);
+    }
 
     @Test
     public void greetsTheUsers() {
@@ -29,21 +34,16 @@ public class DisplayerTests {
 
     @Test
     public void showsTheBoard() {
-        ArrayList board = new ArrayList<Integer>();
-        Collections.addAll(board, 0, 1, 2, 3, 4, 5, 6, 7, 8);
         final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
-        displayer.showBoard(board);
+        displayer.showBoard(board.places);
 
         assertEquals("0 | 1 | 2\n3 | 4 | 5\n6 | 7 | 8\n", outContent.toString());
     }
 
     @Test
     public void returnsPositionGivenByPlayerWhenValid() throws IOException {
-        Board board = new Board();
-        Collections.addAll(board.places, 0, 1, 2, 3, 4, 5, 6, 7, 8);
-        Validator validator = new Validator();
         String input = "0";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
@@ -52,9 +52,6 @@ public class DisplayerTests {
 
     @Test
     public void returnsOnlyTheCorrectValuesWhenGettingAPosition1() throws IOException {
-        Board board = new Board();
-        Collections.addAll(board.places, 0, 1, 2, 3, 4, 5, 6, 7, 8);
-        Validator validator = new Validator();
         final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
         String input = "J";
@@ -69,10 +66,7 @@ public class DisplayerTests {
 
     @Test
     public void returnsOnlyTheCorrectValuesWhenGettingAPosition2() throws IOException {
-        Board board = new Board();
-        Collections.addAll(board.places, 0, 1, 2, 3, 4, 5, 6, 7, 8);
         board.putSignOnBoard("X", 0);
-        Validator validator = new Validator();
         final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
         String input = "0";
@@ -85,6 +79,26 @@ public class DisplayerTests {
         assertEquals(1, displayer.getPosition(board, validator));
     }
 
+    @Test
+    public void returnsOnlyTheCorrectValuesWhenGettingAPosition3() throws IOException {
+        try {
+            board.putSignOnBoard("X", 0);
+            String input = "0";
+            InputStream in = new ByteArrayInputStream(input.getBytes());
+            System.setIn(in);
 
+            displayer.getPosition(board, validator);
+
+            final ByteArrayOutputStream outContentTwo = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(outContentTwo));
+
+            assertEquals("Pick a non-taken number on board", outContentTwo.toString());
+        } finally {
+            String inputTwo = "1";
+            InputStream inTwo = new ByteArrayInputStream(inputTwo.getBytes());
+            System.setIn(inTwo);
+        }
+
+    }
 
 }
