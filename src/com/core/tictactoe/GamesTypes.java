5a827309abc7f1769a67251cb19d9fe161a893d0
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-06-01 16:36:23 +0100

    Fix: removed GamesTypes - unused class.

--- src/com/core/tictactoe/GamesTypes.java
+++
@@ -1,7 +0,0 @@
-package com.core.tictactoe;
-
-public class GamesTypes {
-
-    public static final String HUMAN_VS_HUMAN = "1"; // public, static, final
-    public static final String EXIT = "2";
-}
