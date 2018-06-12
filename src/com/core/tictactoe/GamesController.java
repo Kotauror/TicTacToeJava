34b060edfccbc5a5aa1f60870aa64b4d16ca9267
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-06-12 12:39:52 +0100

    Refactor: rename runProgram to isRunning.

--- src/com/core/tictactoe/GamesController.java
+++ src/com/core/tictactoe/GamesController.java
@@ -6,24 +6,24 @@ import com.core.tictactoe.game_options.GameOptionsFactory;
 public class GamesController {
 
     private CommandLineUi commandLineUi;
-    private Boolean runProgram;
+    private Boolean isRunning;
 
     public GamesController(CommandLineUi commandLineUi) {
         this.commandLineUi = commandLineUi;
-        this.runProgram = true;
+        this.isRunning = true;
     }
 
     public void run() {
-        while (this.runProgram) {
+        while (this.isRunning) {
             this.commandLineUi.gamingMenu();
             String userInput = this.commandLineUi.getUserInput();
             userInput = this.commandLineUi.askWhoGoesFirst(userInput);
             GameOption gameOption = GameOptionsFactory.get(userInput);
-            this.runProgram = gameOption.run(this.commandLineUi);
+            this.isRunning = gameOption.run(this.commandLineUi);
         }
     }
 
     public boolean getRunProgramStatus() {
-        return this.runProgram;
+        return this.isRunning;
     }
 }
