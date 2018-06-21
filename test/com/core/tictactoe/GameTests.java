22cb756bde3164f479193faa534e192045e4c8d0
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-06-21 12:43:28 +0100

    Test: add a test to gameTests for a Human vs Human game on 4x4 board.

--- test/com/core/tictactoe/GameTests.java
+++ test/com/core/tictactoe/GameTests.java
@@ -13,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class GameTests {
 
     private static Game game;
+    private static Board board4x4;
     private static Board board3x3;
     private static Board board2x2;
     private static HumanPlayer humanPlayerX;
@@ -23,6 +24,7 @@ public class GameTests {
 
     @BeforeEach
     void instantiate() {
+        board4x4 = new Board(4);
         board3x3 = new Board(3);
         board2x2 = new Board(2);
         humanPlayerX = new HumanPlayer("X");
@@ -47,7 +49,7 @@ public class GameTests {
     }
 
     @Test
-    void playsAWinningGameOfTwoHumanPlayers() {
+    void playsAWinningGameOfTwoHumanPlayersOnBoard3x3() {
         String[] fakeUsersInputs = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         StubCommandLineUi stubCommandLineUi = new StubCommandLineUi(new PrintStream(outputStream), System.in, fakeUsersInputs);
@@ -64,7 +66,7 @@ public class GameTests {
     }
 
     @Test
-    void playsATieGameOfHumanAndComputer() {
+    void playsATieGameOfHumanAndComputerOnBoard3x3() {
         String[] fakeUsersInputs = {"1", "2", "7", "6", "9"};
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         StubCommandLineUi stubCommandLineUi = new StubCommandLineUi(new PrintStream(outputStream), System.in, fakeUsersInputs);
@@ -81,7 +83,7 @@ public class GameTests {
     }
 
     @Test
-    void playsATieGameOfTwoHumanPlayers() {
+    void playsATieGameOfTwoHumanPlayersOnBoard3x3() {
         String[] fakeUsersInputs = {"2", "1", "4", "3", "5", "6", "7", "8", "9"};
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         StubCommandLineUi stubCommandLineUi = new StubCommandLineUi(new PrintStream(outputStream), System.in, fakeUsersInputs);
@@ -97,7 +99,7 @@ public class GameTests {
     }
 
     @Test
-    void playsATieGameOfTwoComputers() {
+    void playsATieGameOfTwoComputersOnBoard3x3() {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         CommandLineUi commandLineUi = new CommandLineUi(new PrintStream(outputStream), System.in);
         game = new Game(commandLineUi, board3x3, computerPlayerX, computerPlayerO);
@@ -124,6 +126,23 @@ public class GameTests {
         String[] expectedArray = {"X", "O", "X", "4"};
         assertArrayEquals(expectedArray, board2x2.getPlaces());
         assertEquals("X", board2x2.winnerSign());
+    }
+
+    @Test
+    void playsAWinningGameOfTwoHumanPlayersOnBoard4x4() {
+        String[] fakeUsersInputs = {"1", "5", "2", "6", "3", "7", "4", "8"};
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        StubCommandLineUi stubCommandLineUi = new StubCommandLineUi(new PrintStream(outputStream), System.in, fakeUsersInputs);
+        game = new Game(stubCommandLineUi, board4x4, humanPlayerX, humanPlayerO);
+
+        game.run();
+
+        assertTrue(board4x4.isWon());
+        assertFalse(board4x4.isTie());
+        assertEquals("X", board4x4.winnerSign());
+        assertEquals("O", game.getActivePlayer().getSign());
+        String[] expectedArray = {"X", "X", "X", "X", "O", "O", "O", "8", "9", "10", "11", "12", "13", "14", "15", "16"};
+        assertArrayEquals(expectedArray, board4x4.getPlaces());
     }
 }
 
